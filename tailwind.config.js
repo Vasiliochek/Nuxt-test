573136f9745a151d0158a6cb5a755794aa3a0cfa
Vasily Makarov <makarov.vasia@gmail.com>
2024-09-09 15:35:16 +0400

    transfer

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{vue,js}',
+    './nuxt.config.{js,ts}',
+    './app.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
